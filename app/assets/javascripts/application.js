2fdb0e96098ee4cc263d277004ceaa7588e8df7b
Lucas Videla <videla.lucas@gmail.com>
2017-03-29 09:12:09 -0300

    Se agrega bower y dependencias basicas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery/dist/jquery
+//= require bootstrap/dist/js/bootstrap
 //= require_tree .
