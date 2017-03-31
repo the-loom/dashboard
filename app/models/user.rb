cc4dfa2cc8da1767aac362b70db96c0b4adb709c
Lucas Videla <videla.lucas@gmail.com>
2017-03-31 08:54:30 -0300

    Se actualiza avatar con cada ingreso

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
 
+  def self.by_omniauth(auth)
+    user = self.find_by(provider: auth["provider"], uid: auth["uid"]) || self.create_with_omniauth(auth)
+    user.update_attribute(:image, auth['info']['image'])
+    user
+  end
+
+  private
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
@@ -10,5 +17,4 @@ class User < ApplicationRecord
       user.image = auth['info']['image']
     end
   end
-
 end
