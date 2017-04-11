a856c6aa741b17bb88da3b6c7e88b6b17d7aedc8
Lucas Videla <videla.lucas@gmail.com>
2017-04-11 19:30:13 -0300

    Se agregan specs para la asignacion de emblemas

---
+++ spec/models/badge_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Badge, :type => :model do
+  it "is assigned correctly" do
+    user = User.create!(name: 'John', points: 0)
+    badge = Badge.create!(name: 'Neat')
+
+    user.earn(badge)
+
+    expect(user.badges).to include(badge)
+  end
+end
