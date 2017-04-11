d01689f0320bc7e2e7e9a323db7b6cf82f94f0e5
Lucas Videla <videla.lucas@gmail.com>
2017-04-11 19:29:48 -0300

    Se ordenan los eventos y emblemas por el momento en que fueron otorgados

--- app/models/earning.rb
+++ app/models/earning.rb
@@ -1,4 +1,6 @@
 class Earning < ApplicationRecord
   belongs_to :user
   belongs_to :badge
+
+  default_scope { order(created_at: :asc) }
 end
--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,4 +1,6 @@
 class Occurrence < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  default_scope { order(created_at: :asc) }
 end
