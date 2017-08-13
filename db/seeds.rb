a7dbc0a4fd1c3c35977c0d2f3601f602a00916e6
Lucas Videla <videla.lucas@gmail.com>
2017-08-13 19:36:59 -0300

    Se emprolija presentacion de cara a puesta en produccion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(
+        provider: 'github',
+        uid: 684051,
+        name: 'Lucas Videla',
+        nickname: 'delucas',
+        email: 'videla.lucas@gmail.com',
+        image: 'https://avatars2.githubusercontent.com/u/684051?v=3',
+        role: :teacher,
+        enabled: true,
+        locked: true
+)
