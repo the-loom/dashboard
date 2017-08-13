b115a4adc56e7778eda8d63e5e1f12bbc97247ae
Lucas Videla <videla.lucas@gmail.com>
2017-08-13 19:13:04 -0300

    Ordenamiento de revisiones

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,8 +4,8 @@ class Team < ApplicationRecord
   has_many :measurements
   has_many :readings, through: :measurements, source: :reading
 
-  has_many :checks
-  has_many :checkpoints, -> { order(created_at: :desc) }, through: :checks
+  has_many :checks, -> { order(created_at: :desc) }
+  has_many :checkpoints, through: :checks
 
   def self.sorted
     self.order(:name)
