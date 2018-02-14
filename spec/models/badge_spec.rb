fdee11fcbbb992e250ae8bc622845f3e27554fbe
Lucas Videla <videla.lucas@gmail.com>
2018-02-14 10:19:34 -0300

    Se corrigen tests

--- spec/models/badge_spec.rb
+++ spec/models/badge_spec.rb
@@ -2,8 +2,10 @@ require "rails_helper"
 
 RSpec.describe Badge, type: :model do
   it "is assigned correctly" do
-    user = User.create!(name: "John", points: 0)
-    badge = Badge.create!(name: "Neat")
+    Course.current = Course.new(name: "STEM I")
+    user = User.create!(name: "John")
+    user.memberships << Membership.new(course: Course.current, role: :student)
+    badge = Badge.create!(name: "Neat", course: Course.current)
 
     user.earn(badge)
 
