e5e189003970d98c6f32c345660ed91f2feb2cdc
Lucas Videla <videla.lucas@gmail.com>
2018-04-03 10:12:32 -0300

    Rubocop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  BetterErrors::Middleware.allow_ip! '0.0.0.0/0'
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 
 end
