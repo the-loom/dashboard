71b65f8f5c887a81cba52e4e7db8f3ccffb0fc9d
Lucas Videla <videla.lucas@gmail.com>
2018-08-29 20:08:37 -0300

    Fixed compatibility with SQLite3

--- db/migrate/20180829121721_add_enabled_to_membership.rb
+++ db/migrate/20180829121721_add_enabled_to_membership.rb
@@ -2,7 +2,7 @@ class AddEnabledToMembership < ActiveRecord::Migration[5.2]
   def change
     add_column :memberships, :enabled, :boolean, default: true
 
-    execute "update memberships m set enabled = false where m.user_id in (select id from users where enabled = false);"
+    execute "update memberships as m set enabled = false where m.user_id in (select id from users where enabled = false);"
 
     remove_column :users, :enabled
   end
