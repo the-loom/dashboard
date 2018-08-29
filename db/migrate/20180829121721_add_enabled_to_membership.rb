453d8dc23adcfc54772e95a1eeacf07929c74b1f
Lucas Videla <videla.lucas@gmail.com>
2018-08-29 20:50:28 -0300

    Back to normal

--- db/migrate/20180829121721_add_enabled_to_membership.rb
+++ db/migrate/20180829121721_add_enabled_to_membership.rb
@@ -2,7 +2,7 @@ class AddEnabledToMembership < ActiveRecord::Migration[5.2]
   def change
     add_column :memberships, :enabled, :boolean, default: true
 
-    execute "update memberships set enabled = 0 where user_id in (select id from users where enabled = 0);"
+    execute "update memberships set enabled = false where user_id in (select id from users where enabled = false);"
 
     remove_column :users, :enabled
   end
