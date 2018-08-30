ba8afa7eae84100f78327dfff275cedc224fbdfc
Lucas Videla <videla.lucas@gmail.com>
2018-08-29 21:07:14 -0300

    Trying again

--- db/migrate/20180829121721_add_enabled_to_membership.rb
+++ db/migrate/20180829121721_add_enabled_to_membership.rb
@@ -2,7 +2,7 @@ class AddEnabledToMembership < ActiveRecord::Migration[5.2]
   def change
     add_column :memberships, :enabled, :boolean, default: true
 
-    execute "update memberships set enabled = false where user_id in (select id from users where enabled = false);"
+    execute "update memberships set enabled = FALSE where user_id in (select id from users where enabled = FALSE);"
 
     remove_column :users, :enabled
   end
