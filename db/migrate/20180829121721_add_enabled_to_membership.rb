416f00002674ca94e88457d138080108f15cc219
Lucas Videla <videla.lucas@gmail.com>
2018-08-29 21:11:49 -0300

    I feel dirty...

--- db/migrate/20180829121721_add_enabled_to_membership.rb
+++ db/migrate/20180829121721_add_enabled_to_membership.rb
@@ -2,7 +2,7 @@ class AddEnabledToMembership < ActiveRecord::Migration[5.2]
   def change
     add_column :memberships, :enabled, :boolean, default: true
 
-    execute "update memberships set enabled = FALSE where user_id in (select id from users where enabled = FALSE);"
+    execute "update memberships set enabled = (1=0) where user_id in (select id from users where enabled = (1=0));"
 
     remove_column :users, :enabled
   end
