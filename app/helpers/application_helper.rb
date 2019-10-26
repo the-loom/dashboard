f8cdfd529ab7fae6527d901f0060813d0bfa4452
Lucas Videla <videla.lucas@gmail.com>
2019-10-26 18:22:24 -0300

    Fallback to user image for listings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,10 @@ module ApplicationHelper
   def entity_avatar(entity)
     if entity.avatar.attached?
       entity.avatar
-    else
-      'https://avatars1.githubusercontent.com/u/20347933?s=400&v=4'
+    elsif entity.respond_to?(:image) && entity.image.present?
+      entity.image
+    else # fallback
+      "https://avatars1.githubusercontent.com/u/20347933?s=400&v=4"
     end
   end
 
