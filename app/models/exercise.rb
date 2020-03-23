58750931f626e3391240443b7cd5d93cbc31d993
Fabian <falago@gmail.com>
2020-03-23 00:17:32 -0300

    Se agrego validacion para la difiultad de un ejercicio

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,6 +4,13 @@ class Exercise < ApplicationRecord
 
   validates_presence_of :name, :notes, :difficulty
   validates :name, uniqueness: { scope: :course_id }
+  validate :difficulty_range
+
+  def difficulty_range
+    if difficulty.present? and (difficulty < 1 or difficulty > 5)
+      errors.add(:difficulty, "cannot be less than 1 or greater than 5")
+    end
+  end
 
   default_scope { order(name: :asc) }
   scope :published, -> { where(published: true) }
