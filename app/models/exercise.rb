d023d3a47fb9da4d0d8b6e4261bc998bc5b9ceb2
Fabian <falago@gmail.com>
2020-03-25 11:28:37 -0300

    Se cambio el mensaje de error de la dificultad

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,7 +4,7 @@ class Exercise < ApplicationRecord
 
   validates_presence_of :name, :notes, :difficulty
   validates :name, uniqueness: { scope: :course_id }
-  validates :difficulty, inclusion: { in: 1..5, message: "cannot be less than 1 or greater than 5" }
+  validates :difficulty, inclusion: { in: 1..5, message: "must be between 1 and 5" }
 
   default_scope { order(name: :asc) }
   scope :published, -> { where(published: true) }
