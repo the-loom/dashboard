0e61327bd25c9e9abb4d453c6a7fa55f50d4d701
Fabian <falago@gmail.com>
2020-03-25 11:30:21 -0300

    Se cambio el mensaje de error de la dificultad

--- app/models/peer_review/challenge.rb
+++ app/models/peer_review/challenge.rb
@@ -4,7 +4,7 @@ class PeerReview::Challenge < ApplicationRecord
 
   validates_presence_of :title, :difficulty, :instructions,
                         :reviewer_instructions
-  validates :difficulty, inclusion: { in: 1..5, message: "cannot be less than 1 or greater than 5" }
+  validates :difficulty, inclusion: { in: 1..5, message: "must be between 1 and 5" }
   
   has_many :solutions, foreign_key: :peer_review_challenge_id
   has_many :reviews, through: :solutions
