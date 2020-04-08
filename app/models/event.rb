982ace2e707d9fb4d0e595694867de94bf1c3b90
Fabian <falago@gmail.com>
2020-04-08 14:56:03 -0300

    Se agrega validacion a los puntos de un evento

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
 
   validates_presence_of :name, :description, :points, :min_points, :max_points
   validates :name, uniqueness: { scope: :course_id }
+  validate :points_min_and_max
 
   scope :enabled, -> { where(enabled: true) }
 
@@ -10,6 +11,21 @@ class Event < ApplicationRecord
   has_many :users, through: :occurrences
 
   belongs_to :competence_tag, optional: true
+
+  def points_min_and_max
+    if self.points.present? && self.points < 0
+      errors.add(:points, "cannot be less than 0")
+    end
+    if self.min_points.present? && self.min_points < 0
+      errors.add(:min_points, "cannot be less than 0")
+    end
+    if self.max_points.present? && self.max_points < 0
+      errors.add(:max_points, "cannot be less than 0")
+    end
+    if self.min_points.present? && self.max_points.present? && self.min_points > self.max_points
+      errors.add(:min_points, "cannot be greater than max points")
+    end  
+  end  
 
   def self.min_points
     Event.enabled.sum(:min_points)
