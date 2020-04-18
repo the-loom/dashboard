aecb9e4469d6beaf3c1a4386e9e3e4839a42c8d4
Fabian <falago@gmail.com>
2020-04-18 01:26:59 -0300

    Se agrega well en desafios y se modifica el helper de rouge

--- app/helpers/rouge_helper.rb
+++ app/helpers/rouge_helper.rb
@@ -1,18 +1,15 @@
 module RougeHelper
   def rouge(text, language)
-      text.gsub(/(<pre>(.*?)<\/pre>)/m) do
-      content = CGI.unescapeHTML($2)
-      content = formatter.format(lexer(language).lex(content))
-      content.gsub('err', 'nf')
-    end
+    formatter.format(lexer(language).lex(text))
   end
+  
   private
 
-    def formatter
-      Rouge::Formatters::HTMLLegacy.new(css_class: 'highlight')
-    end
+  def formatter
+    Rouge::Formatters::HTMLLegacy.new(css_class: 'highlight')
+  end
 
-    def lexer language
-      Rouge::Lexer.find(language)
-    end
+  def lexer language
+    Rouge::Lexer.find(language)
+  end
 end
