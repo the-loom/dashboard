da9feafc3ee1c8a9e4124b530f52cc37bfce3d16
Lucas Videla <videla.lucas@gmail.com>
2020-04-26 09:47:25 -0300

    Disabled feature. Enhanced with transaction and discard

--- app/services/profile_joiner.rb
+++ app/services/profile_joiner.rb
@@ -4,24 +4,44 @@ class ProfileJoiner
   end
 
   def execute
-    @students.each do |student|
-      student.earn(@badge)
+    5/0 # temporarily disabled on purpose
+
+    User.transaction do
+      keepable_student = @students.first
+
+      # 1.a joins identities under the first user
+      identities = @students.map(&:identities).flatten
+      keepable_student.identities << identities
+
+      # 1.b joins memberships under the first user
+      courses = @students.map(&:courses).uniq
+      courses.each do |course|
+        if keepable_student.memberships.where(course: course).empty?
+          keepable_student.memberships << Membership.create(course: course, role: :student)
+        end
+      end
+
+      deletable_students = @students - [keepable_student]
+
+      deletable_students.each do |deletable_student|
+        # remove memberships
+        deletable_student.memberships.discard_all
+
+        # reassign solutions
+        deletable_student.peer_review_solutions.update_all(author_id: keepable_student.id)
+
+        # reassign reviews
+        deletable_student.peer_review_reviews.update_all(reviewer_id: keepable_student.id)
+
+        # discarding users
+        deletable_student.discard!
+      end
+
+      # TODO(later, if needed)
+      # 2. copy points from membership?
+      # 3. copy team from membership?
+      # 4. copy attendance?
     end
-
-    student = @students.first
-
-    # 1. joins identities under the first user
-    identities = @students.map(&:identities).flatten
-    student.identities << identities
-
-    (@students - [student]).each do |s|
-      s.destroy
-    end
-
-    # TODO(later, if needed)
-    # 2. copy points from membership?
-    # 3. copy team from membership?
-    # 4. copy attendance?
 
     true
   end
