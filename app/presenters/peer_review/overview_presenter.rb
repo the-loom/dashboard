dd247978c7025b018cb5218fe41ba25c14908741
Lucas Videla <videla.lucas@gmail.com>
2020-05-08 10:33:21 -0300

    Changes misleading totals in challenge overview

--- app/presenters/peer_review/overview_presenter.rb
+++ app/presenters/peer_review/overview_presenter.rb
@@ -2,11 +2,13 @@ class PeerReview::OverviewPresenter
   attr_reader :registers
 
   def initialize(challenge)
+    solutions = challenge.solutions.final.size
+    reviews = challenge.reviews.final.size
     @registers = [
-        OpenStruct.new(name: "Soluciones", value: challenge.solutions.size),
-        OpenStruct.new(name: "Revisiones", value: challenge.reviews.size),
-        OpenStruct.new(name: "Revisiones por ejercicio", value: challenge.solutions.size > 0 ? (challenge.reviews.size.to_f / challenge.solutions.size).round(2) : 0),
-        OpenStruct.new(name: "Revisiones potenciales", value: challenge.solutions.size * (challenge.solutions.size - 1))
+        OpenStruct.new(name: "Soluciones finalizadas", value: solutions),
+        OpenStruct.new(name: "Revisiones finalizadas", value: reviews),
+        OpenStruct.new(name: "Revisiones por ejercicio", value: solutions > 0 ? (reviews.to_f / solutions).round(2) : 0),
+        OpenStruct.new(name: "Revisiones potenciales", value: solutions * (solutions - 1))
     ]
   end
 end
