817863bf22a9cfafdbf774cd21ab85bc1c4c6080
Lucas Videla <videla.lucas@gmail.com>
2020-05-22 10:13:20 -0300

    Small editions

--- db/migrate/20200521144859_add_optional_to_challenges.rb
+++ db/migrate/20200521144859_add_optional_to_challenges.rb
@@ -1,4 +1,4 @@
-class AddOptionalToChallengues < ActiveRecord::Migration[5.2]
+class AddOptionalToChallenges < ActiveRecord::Migration[5.2]
   def change
     add_column :peer_review_challenges, :optional, :boolean, default: false
   end
