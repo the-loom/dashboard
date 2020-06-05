53043684dda3e06855762bad49b6683bc6f9b66e
Lucas Videla <videla.lucas@gmail.com>
2020-06-05 19:22:28 -0300

    Reorganized code

--- app/presenters/menu_presenter.rb
+++ app/presenters/menu_presenter.rb
@@ -18,60 +18,61 @@ class MenuPresenter
     end
 
     if manage?(User) && manage?(Team) && manage?(Lecture)
-      node = MenuNode.new("Aula", [])
-      node.leafs << MenuLeaf.new("Docentes", route.teachers_path)
-      node.leafs << MenuLeaf.new("Estudiantes", route.students_path)
-      node.leafs << MenuLeaf.new("Equipos", route.teams_path) if on?(:teams)
+      classroom_node = MenuNode.new("Aula")
+      classroom_node << MenuLeaf.new("Docentes", route.teachers_path)
+      classroom_node << MenuLeaf.new("Estudiantes", route.students_path)
+      classroom_node << MenuLeaf.new("Equipos", route.teams_path) if on?(:teams)
       if on?(:lectures)
-        node.leafs << MenuLeaf.new("Clases", route.lectures_path)
-        node.leafs << MenuSeparator.new
-        node.leafs << MenuLeaf.new("Resumen de Asistencia", route.overview_lectures_path)
+        classroom_node << MenuLeaf.new("Clases", route.lectures_path)
+        classroom_node << MenuSeparator.new
+        classroom_node << MenuLeaf.new("Resumen de Asistencia", route.overview_lectures_path)
       end
-      menu << node
+      menu << classroom_node
     end
 
     if on?(:events) || on?(:badges) || on?(:competences)
-      node = MenuNode.new("Gamificación", [])
-      node.leafs << MenuLeaf.new("Eventos", route.events_path) if on?(:events)
-      node.leafs << MenuLeaf.new("Competencias", route.competence_tags_path) if on?(:competences)
-      node.leafs << MenuLeaf.new("Emblemas", route.badges_path) if on?(:badges)
-      node.leafs << MenuSeparator.new
-      node.leafs << MenuLeaf.new("Estadísticas", route.points_stats_path)
-      menu << node
+      gamification_node = MenuNode.new("Gamificación")
+      gamification_node << MenuLeaf.new("Eventos", route.events_path) if on?(:events)
+      gamification_node << MenuLeaf.new("Competencias", route.competence_tags_path) if on?(:competences)
+      gamification_node << MenuLeaf.new("Emblemas", route.badges_path) if on?(:badges)
+      gamification_node << MenuSeparator.new
+      gamification_node << MenuLeaf.new("Estadísticas", route.points_stats_path)
+      menu << gamification_node
     end
 
     menu << MenuLeaf.new("Tablero", route.dashboard_index_path)
     menu << MenuLeaf.new("Recursos", route.resources_path) if on?(:resources)
 
     if on?(:multiple_choices) || on?(:tiny_cards) || on?(:automatic_correction_challenges) || on?(:peer_review_challenges) || on?(:exercises)
-      node = MenuNode.new("Ejercitación", [])
-      node.leafs << MenuLeaf.new("Tarjetas", route.tiny_cards_decks_path) if on?(:tiny_cards)
-      node.leafs << MenuLeaf.new("Preguntas de Opción Múltiple", route.multiple_choices_questionnaires_path) if on?(:multiple_choices)
-      node.leafs << MenuLeaf.new("Desafíos de Corrección Automática", route.repos_path) if on?(:automatic_correction_challenges)
-      node.leafs << MenuLeaf.new("Desafíos de Revisión", route.peer_review_challenges_path) if on?(:peer_review_challenges)
-      node.leafs << MenuLeaf.new("Ejercicios", route.exercises_path) if on?(:exercises)
-      menu << node
+      exercises_node = MenuNode.new("Ejercitación")
+      exercises_node << MenuLeaf.new("Tarjetas", route.tiny_cards_decks_path) if on?(:tiny_cards)
+      exercises_node << MenuLeaf.new("Preguntas de Opción Múltiple", route.multiple_choices_questionnaires_path) if on?(:multiple_choices)
+      exercises_node << MenuLeaf.new("Desafíos de Corrección Automática", route.repos_path) if on?(:automatic_correction_challenges)
+      exercises_node << MenuLeaf.new("Desafíos de Revisión", route.peer_review_challenges_path) if on?(:peer_review_challenges)
+      exercises_node << MenuLeaf.new("Ejercicios", route.exercises_path) if on?(:exercises)
+      menu << exercises_node
     end
     menu << MenuNotification.new(@current_user, route.notifications_path)
 
-    courses_node = MenuNode.new(Course.current.name, [])
+    courses_node = MenuNode.new(Course.current.name)
     if @current_user.teacher?
       courses_for_user = @current_user.all_courses
     else
       courses_for_user = @current_user.courses
     end
     courses_for_user.order(enabled: :desc, name: :asc).each do |course|
-      courses_node.leafs << MenuLeaf.new(course.name, route.switch_course_path(course), (!course.enabled? ? "text-decoration: line-through" : nil))
+      courses_node << MenuLeaf.new(course.name, route.switch_course_path(course), (!course.enabled? ? "text-decoration: line-through" : nil))
     end
-    courses_node.leafs << MenuSeparator.new
-    courses_node.leafs << MenuLeaf.new("Otros cursos", route.courses_path)
+    courses_node << MenuSeparator.new
+    courses_node << MenuLeaf.new("Otros cursos", route.courses_path)
     menu << courses_node
 
-    user_node = MenuNode.new(@current_user.short_name, [])
-    user_node.leafs << MenuLeaf.new("Perfil", route.profile_path)
-    user_node.leafs << MenuLeaf.new("Editar perfil", route.edit_profile_path)
-    user_node.leafs << MenuSeparator.new
-    user_node.leafs << MenuLeaf.new("Salir", route.logout_path)
+    user_node = MenuNode.new(@current_user.short_name, [
+        MenuLeaf.new("Perfil", route.profile_path),
+        MenuLeaf.new("Editar perfil", route.edit_profile_path),
+        MenuSeparator.new,
+        MenuLeaf.new("Salir", route.logout_path)
+    ])
     menu << user_node
 
     menu
@@ -93,9 +94,12 @@ end
 
 class MenuNode
   attr_reader :title, :leafs
-  def initialize (title, leafs)
+  def initialize (title, leafs = [])
     @title = title
     @leafs = leafs
+  end
+  def <<(leaf)
+    @leafs << leaf
   end
 end
 
