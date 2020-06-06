2a38fbfcaa7567e54ab2c1fa5ae4989dc0ff3fa8
Lucas Videla <videla.lucas@gmail.com>
2020-06-06 18:59:07 -0300

    Reorganized PeerReview::Challenge show screen (#309)

--- app/services/solution_finder.rb
+++ app/services/solution_finder.rb
@@ -7,7 +7,7 @@ class SolutionFinder
   def find_solution
     # 1. if myself only, as usual
     unless @challenge.team_challenge
-      return PeerReview::Solution.find_or_create_by(challenge: @challenge, author: @user)
+      return PeerReview::Solution.find_or_initialize_by(challenge: @challenge, author: @user)
     end
 
     # 2. if teams, lookup for team's solution
@@ -16,7 +16,7 @@ class SolutionFinder
       challenge = PeerReview::Solution.find_by(challenge: @challenge, author: @user.current_membership.team.members)
     end
     # 3. if none, create new
-    challenge ||= PeerReview::Solution.create(challenge: @challenge, author: @user)
+    challenge ||= PeerReview::Solution.new(challenge: @challenge, author: @user)
     challenge
   end
 end
