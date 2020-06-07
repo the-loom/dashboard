db683649bda38d4750673fd29ea28882a08cc582
Lucas Videla <videla.lucas@gmail.com>
2020-06-07 19:07:54 -0300

    Hotfix: Show Team Challenge for users without Team

--- app/services/solution_finder.rb
+++ app/services/solution_finder.rb
@@ -12,7 +12,7 @@ class SolutionFinder
 
     # 2. if teams, lookup for team's solution
     if @challenge.team_challenge
-      return nil unless @user.current_membership.team
+      return OpenStruct.new(new_record?: true) unless @user.current_membership.team
       challenge = PeerReview::Solution.find_by(challenge: @challenge, author: @user.current_membership.team.members)
     end
     # 3. if none, create new
