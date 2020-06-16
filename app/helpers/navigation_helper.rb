035022611cc103f837a64a63964fd44ea748c3d5
Lucas Videla <videla.lucas@gmail.com>
2020-06-16 09:33:05 -0300

    UI enhancements for page titles (#323)
    * Uses breadcrumbs data in order to build titles
    
    * Better title calculations

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -7,11 +7,20 @@ module NavigationHelper
     nav_ensure << { title: title, url: url }
   end
 
+  # Bootstrap 3
   def nav_render
+    set_title
     render partial: "shared/navigation", locals: { nav: nav_ensure }
   end
 
+  # Bootstrap 4
   def nav_render4
+    set_title
     render partial: "shared/navigation4", locals: { nav: nav_ensure }
   end
+
+  private
+    def set_title
+      @title = nav_ensure.reverse.map { |nav| nav[:title] }
+    end
 end
