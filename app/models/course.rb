a8deac4e0be5c471b8d5afa0167cc2893cd08811
Lucas Videla <videla.lucas@gmail.com>
2020-09-03 10:25:30 -0300

    Fixed issues with duplicate course

--- app/models/course.rb
+++ app/models/course.rb
@@ -19,50 +19,63 @@ class Course < ApplicationRecord
   scope :enabled, -> { where(enabled: true) }
 
   def fully_duplicate!
-    new_course = self.dup
-    new_course.name = "Copia de " + self.name
+    new_course = nil
 
-    Event.unscoped.where(course: self).each do |event|
-      new_event = event.dup
-      new_event.course = new_course
-      new_event.enabled = false
-      new_event.save
+    Course.transaction do
+      new_course = self.dup
+      new_course.name = "Copia de " + self.name
+      new_course.save!
+
+      old = Course.current
+      Course.current = new_course
+
+      Event.unscoped.where(course: self).each do |event|
+        new_event = event.dup
+        new_event.course = new_course
+        new_event.enabled = false
+        new_event.save!
+      end
+
+      # categories!
+      Resource.unscoped.where(course: self).each do |resource|
+        new_resource = resource.dup
+        new_resource.course = new_course
+        current_resource_category = ResourceCategory.unscoped.find(resource.resource_category_id)
+        new_resource.resource_category = ResourceCategory.unscoped.find_or_create_by(course: new_course, name: current_resource_category.name)
+        new_resource.save!
+      end
+
+      due_date_offset = nil
+      PeerReview::Challenge.unscoped.where(course: self).order(due_date: :asc).each do |challenge|
+        due_date_offset ||= (Time.zone.now.to_date - challenge.due_date).to_i + 30 if challenge.due_date
+        new_challenge = challenge.dup
+        new_challenge.course = new_course
+        new_challenge.enabled = true
+        new_challenge.published = false
+        new_challenge.due_date = challenge.due_date + due_date_offset if challenge.due_date
+        new_challenge.save!
+      end
+
+      Exercise.unscoped.where(course: self).each do |exercise|
+        new_exercise = exercise.dup
+        new_exercise.course = new_course
+        new_exercise.published = false
+        new_exercise.save!
+      end
+
+      # TODO(delucas): left for the next iteration
+      # copy automatic correction challenges
+      # copy badges
+      # copy competences
+      # copy cards
+      # copy lectures
+      # copy multiple choices
+
+      new_course.save!
+
+      Course.current = old
     end
 
-    # categories!
-    Resource.unscoped.where(course: self).each do |resource|
-      new_resource = resource.dup
-      new_resource.course = new_course
-      new_resource.resource_category = ResourceCategory.unscoped.find_or_create_by(course: new_course, name: resource.resource_category.name)
-      new_resource.save
-    end
-
-    due_date_offset = nil
-    PeerReview::Challenge.unscoped.where(course: self).order(due_date: :asc).each do |challenge|
-      due_date_offset ||= (Time.zone.now.to_date - challenge.due_date).to_i + 30
-      new_challenge = challenge.dup
-      new_challenge.course = new_course
-      new_challenge.enabled = false
-      new_challenge.due_date = challenge.due_date + due_date_offset
-      new_challenge.save
-    end
-
-    Exercise.unscoped.where(course: self).each do |exercise|
-      new_exercise = exercise.dup
-      new_exercise.course = new_course
-      new_exercise.published = false
-      new_exercise.save
-    end
-
-    # TODO(delucas): left for the next iteration
-    # copy automatic correction challenges
-    # copy badges
-    # copy competences
-    # copy cards
-    # copy lectures
-    # copy multiple choices
-
-    new_course.save!
     new_course
   end
 
