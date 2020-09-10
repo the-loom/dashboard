8bfe6ec164af1e33b2fc88cbdace73bf8574a258
Lucas Videla <videla.lucas@gmail.com>
2020-09-10 20:54:48 -0300

    Fixes course not found 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,8 +28,8 @@ class ApplicationController < ActionController::Base
       accessor = instance_variable_get(:@_request)
       ActiveRecord::Base.send(:define_method, "session", proc { accessor.session })
       Course.current = Course.find_by(id: session[:course_id]) if session[:course_id].present? && Course.enabled.map(&:id).include?(session[:course_id])
-      redirect_to courses_url unless Course.current
       User.current = current_user
+      redirect_to courses_url if User.current && !Course.current
     end
 
     def verify_name
