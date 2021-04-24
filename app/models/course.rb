fac8a533448a1a8f92c388c29e9419be7c9c5f24
Lucas Videla <videla.lucas@gmail.com>
2021-04-24 11:40:42 -0300

    Duplicate a course now duplicates MultipleChoices

--- app/models/course.rb
+++ app/models/course.rb
@@ -72,7 +72,23 @@ class Course < ApplicationRecord
       # copy competences
       # copy cards
       # copy lectures
-      # copy multiple choices
+
+      MultipleChoices::Questionnaire.unscoped.where(course: self).each do |questionnaire|
+        new_questionnaire = questionnaire.dup
+        new_questionnaire.course = new_course
+
+        questionnaire.questions.unscoped.where(multiple_choices_questionnaire_id: questionnaire.id).each do |question|
+          new_question = question.dup
+          question.answers.unscoped.where(multiple_choices_question_id: question.id).each do |answer|
+            new_question.answers << answer.dup
+          end
+          new_questionnaire.questions << new_question
+        end
+
+        new_questionnaire.enabled = true
+        new_questionnaire.published = false
+        new_questionnaire.save!
+      end
 
       new_course.save!
 
