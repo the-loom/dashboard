439809856fa224a007903ae6b36cf8c74ad44370
Lucas Videla <videla.lucas@gmail.com>
2021-05-15 13:12:07 -0300

    Fixed competence points calculations and normalization process

--- app/presenters/competence_tags_stats.rb
+++ app/presenters/competence_tags_stats.rb
@@ -4,7 +4,7 @@ class CompetenceTagsStats
   def initialize
     @values = Hash.new(0)
 
-    Event.all.includes(:competence_tag).group_by do |event|
+    Event.enabled.includes(:competence_tag).group_by do |event|
       event.competence_tag
     end.each do |group, events|
       next unless group
@@ -15,10 +15,10 @@ class CompetenceTagsStats
   end
 
   def normalized
-    baseline = CompetenceTagsStats.new.values
-    @values.sort.map do |key, value|
-      divisor = baseline[key] == 0 ? (value == 0 ? 1 : value) : baseline[key]
-      { axis: key, value: [value / divisor.to_f, 1].min.round(2) }
+    CompetenceTagsStats.new.values.sort.map do |key, base_value|
+      actual_value = @values[key]
+      divisor = base_value == 0 ? (actual_value == 0 ? 1 : actual_value) : base_value
+      { axis: key, value: [actual_value / divisor.to_f, 1].min.round(2) }
     end
   end
 end
