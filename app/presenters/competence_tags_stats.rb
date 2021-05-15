926839ec0fcc06a17802aa35c48688c338329368
Lucas Videla <videla.lucas@gmail.com>
2021-05-15 11:14:22 -0300

    Fixed members without group for competence stats

--- app/presenters/competence_tags_stats.rb
+++ app/presenters/competence_tags_stats.rb
@@ -7,6 +7,7 @@ class CompetenceTagsStats
     Event.all.includes(:competence_tag).group_by do |event|
       event.competence_tag
     end.each do |group, events|
+      next unless group
       @values[group.name] = events.inject(0) do |total_points, event|
         total_points + event.max_points
       end
