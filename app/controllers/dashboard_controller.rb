714394230a3738df52d58fb7612c00e69695f9a1
Lucas Videla <videla.lucas@gmail.com>
2021-06-21 18:10:28 -0300

    Added published exercises to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   layout "application2"
 
   def index
+    @exercises = Exercise.published
     @challenges = PeerReview::Challenge.published
     @questionnaires = MultipleChoices::Questionnaire.published
   end
