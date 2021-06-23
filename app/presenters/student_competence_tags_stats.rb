73e671f8072e30caae6cbc4ed7fb177c980d55d6
Lucas Videla <videla.lucas@gmail.com>
2021-06-23 08:58:51 -0300

    Fixed for courses without consistent competence tagging

--- app/presenters/student_competence_tags_stats.rb
+++ app/presenters/student_competence_tags_stats.rb
@@ -3,7 +3,7 @@ class StudentCompetenceTagsStats < CompetenceTagsStats
     @values = Hash.new(0)
     return unless student.enabled?
 
-    student.occurrences.group_by do |occurrence|
+    student.occurrences.joins(:event).where.not({ occurrences: { events: { competence_tag: nil } } }).group_by do |occurrence|
       occurrence.event.competence_tag
     end.each do |competence, occurrences|
       @values[competence.name] = occurrences.inject(0) do |total_points, occurrence|
