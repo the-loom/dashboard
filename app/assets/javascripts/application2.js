d37d8f84d772833258ace30cfca061c9034d86df
Lucas Videla <videla.lucas@gmail.com>
2021-06-28 09:24:26 -0300

    Se mejora pantalla de docentes

--- app/assets/javascripts/application2.js
+++ app/assets/javascripts/application2.js
@@ -4,3 +4,15 @@
 //= require codemirror/mode/clike/clike
 //= require codemirror/mode/python/python
 //= require radarChart
+
+function equalizeHeights(selector) {
+    var maxHeight = 0;
+    $(selector).height('auto');
+    $(selector).each(function () {
+        var thisH = $(this).height();
+        if (thisH > maxHeight) {
+            maxHeight = thisH;
+        }
+    });
+    $(selector).height(maxHeight);
+}
