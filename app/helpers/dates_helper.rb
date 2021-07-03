4b4dab592893ae41d6f633c0e3ad441f3b4b909c
Lucas Videla <videla.lucas@gmail.com>
2021-07-03 11:06:30 -0300

    Se mejoran los helpers para time_range

--- app/helpers/dates_helper.rb
+++ app/helpers/dates_helper.rb
@@ -8,4 +8,9 @@ module DatesHelper
     return "N/A" unless time.present?
     time.in_time_zone("Buenos Aires").strftime("%H:%M")
   end
+
+  def display_time_range(time_from, time_to)
+    return "N/A" unless time_from.present? && time_to.present?
+    "#{display_time time_from} a #{display_time time_to}"
+  end
 end
